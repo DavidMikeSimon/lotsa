9cbef14dc61a6b1d7e35cd4313be191229383cfd
David Simon <davidsimon@swellinvesting.com>
2019-03-30 09:07:55 -0700

    Using webpack

--- lotsa-server/src/main.rs
+++ lotsa-server/src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{server, App, fs};
 
 pub fn main() {
   server::new(|| {
-    App::new().handler("/", fs::StaticFiles::new("www").unwrap())
+    App::new().handler("/", fs::StaticFiles::new("www/dist").unwrap().index_file("index.html"))
   })
     .bind("127.0.0.1:8088")
     .unwrap()
