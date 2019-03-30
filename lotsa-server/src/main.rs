ea678435c1e690b9132642822594dbe7d7d4573c
David Simon <davidsimon@swellinvesting.com>
2019-03-29 20:56:13 -0700

    wip serving wasm app through actix

--- lotsa-server/src/main.rs
+++ lotsa-server/src/main.rs
@@ -1,34 +1,9 @@
-#[macro_use]
-extern crate maplit;
-
-use actix_web::{server, App, HttpRequest};
-
-use lotsa::{
-  block::{BlockType, EMPTY, UNKNOWN},
-  chunk::Chunk,
-  debug::Debugger,
-};
-
-pub const STUFF: BlockType = BlockType(3);
-
-fn index(_req: &HttpRequest) -> String {
-  let mut chunk = Chunk::new();
-  let debugger = Debugger::new(hashmap!(UNKNOWN => 'X', EMPTY => '.', STUFF => 'S'));
-
-  debugger.load(
-    &mut chunk,
-    ".....
-     .....
-     .SSS.
-     .....
-     .....",
-  );
-
-  debugger.dump(&chunk)
-}
+use actix_web::{server, App, fs};
 
 pub fn main() {
-  server::new(|| App::new().resource("/", |r| r.f(index)))
+  server::new(|| {
+    App::new().handler("/", fs::StaticFiles::new("www").unwrap())
+  })
     .bind("127.0.0.1:8088")
     .unwrap()
     .run()
